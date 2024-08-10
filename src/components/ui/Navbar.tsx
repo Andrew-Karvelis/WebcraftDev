3610e80d97d3ac943dcd04d93e0d3af1d7393d40
Andrew-Karvelis <akarvelis.work@gmail.com>
2024-08-10 13:39:55 +1000

    (ADD) Clerk login/logout auth + UI fixes

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../../public/logo.png";
+import {
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-around items-center bg-[#242424] w-full h-[68px] fixed text-white z-10">
+      <div className="flex gap-2">
+        <figure className="w-[48px] ml-4 sm:w-[40px] sm:h-[40px]">
+          <Image src={logo} alt="WebCraft Logo" className="invert" />
+        </figure>
+        <input
+          className="rounded-sm mt-1 h-[36px] text-black  pl-1"
+          placeholder="Search..."
+        />
+      </div>
+      <SignedIn>
+        <div className="flex gap-2">
+          <UserButton />
+          <SignOutButton />
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </nav>
+  );
+}
